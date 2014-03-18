01d1b0042cca7836597743472a58992be0efbac6
Moirae <is.moirae@gmail.com>
2014-03-18 16:09:03 +0100

    Sub-plugin start

---
+++ verification/index.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Book verification
+ *
+ * @package    bookverification
+ * @copyright  2014 Ivana Skelic, Hrvoje Golcic 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require(dirname(__FILE__).'/../../../../config.php');
+require_once(dirname(__FILE__).'/locallib.php');
+
+$id 		= required_param('id', PARAM_INT);				// Course Module ID
+$chapterid	= optional_param('chapterid', 0, PARAM_INT);	// Chapter ID
+
+// =========================================================================
+// security checks START - teachers and students view
+// =========================================================================
+
+$cm = get_coursemodule_from_id('book', $id, 0, false, MUST_EXIST);
+$course = $DB->get_record('course', array('id'=>$cm->course), '*', MUST_EXIST);
+$book = $DB->get_record('book', array('id'=>$cm->instance), '*', MUST_EXIST);
+
+require_course_login($course, true, $cm);
+
+
+
+
