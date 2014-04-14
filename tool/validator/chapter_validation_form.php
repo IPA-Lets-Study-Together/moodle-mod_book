0c0cbf55f748fd0114b595d67aa58380adf764b4
Moirae <is.moirae@gmail.com>
2014-04-14 19:00:34 +0200

    Install plugin

---
+++ tool/validator/chapter_validation_form.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Book validation
+ *
+ * @package    booktool_validator
+ * @copyright  2014 Ivana Skelic, Hrvoje Golcic 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+require_once($CFG->libdir.'/formslib.php');
+require_once(dirname(__FILE__).'/locallib.php');
+
+class chapter_validation_form extends moodleform {
+
+	function definition() {
+		global $CFG;
+
+		$book = $this->_customdata['book'];
+		$chapter = $this->_customdata['chapter'];
+
+		$mform = $this->_form;
+
+		$mform->addElement('text', 'title', get_string('chaptertitle', 'mod_book'), array('size'=>'30'));
+        $mform->setType('title', PARAM_RAW);
+        $mform->addRule('title', null, 'required', null, 'client');
+		
+
+		$buttonarray=array();
+		$buttonarray[] =& $mform->createElement('submit', 'validate', get_string('validatex'));
+		$buttonarray[] =& $mform->createElement('submit', 'cancel', get_string('cancel'));
+		
+		$mform->addGroup($buttonarray, 'buttonar', '', array(' '), false);
+
+
+	}
+}
---
+++ tool/validator/lang/en/booktool_validator.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * validator booktool language strings
+ *
+ * @package    booktool_validator
+ * @copyright  2014 Ivana Skelic, Hrvoje Golcic 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$string['event_chapter_validated'] = 'There are no validation issues in this chapter';
+$string['event_chapter_notvalidated'] = 'This chapter has validation issues';
+$string['event_book_validated'] = 'There are no validation issues in this book';
+$string['event_book_notvalidated'] = 'This book has validation issues';
+$string['validatechapter'] = 'Validate this chapter';
+$string['validatebook'] = 'Validate book';
+$string['pluginname'] = 'Book content validation';
+$string['validator:validated'] = 'validate';
+
+$string['buttonvalidate'] = 'Validate';
+$string['chapter'] = 'Chapter';
+$string['table'] ='tables';
+$string['image'] = 'images';
+$string['notvalidated'] = 'are not validated.';
