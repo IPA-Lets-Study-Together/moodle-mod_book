1bc4ba0ad1399e3b0c1f25f1e385fa7fd65ced8f
Moirae <is.moirae@gmail.com>
2014-04-18 17:36:06 +0200

    bindex change logics

--- tool/validator/bindex.php
+++ tool/validator/bindex.php
@@ -27,8 +27,7 @@ require_once(dirname(__FILE__).'/locallib.php');
 require_once(dirname(__FILE__).'/validation_form.php');
 require_once(dirname(__FILE__).'/chapter_validation_form.php');
 
-$id        = required_param('id', PARAM_INT);           // Course Module ID
-$chapterid = optional_param('chapterid', 0, PARAM_INT); // Chapter ID
+$id = required_param('id', PARAM_INT);           // Course Module ID
 
 $cm = get_coursemodule_from_id('book', $id, 0, false, MUST_EXIST);
 $course = $DB->get_record('course', array('id'=>$cm->course), '*', MUST_EXIST);
@@ -40,9 +39,18 @@ $context = context_module::instance($cm->id);
 require_capability('mod/book:read', $context);
 require_capability('booktool/validator:validate', $context);
 
-$PAGE->set_url('/mod/book/tool/validator/bindex.html', array('id'=>$id, 'chapterid'=>$chapterid));
+$PAGE->set_url('/mod/book/tool/validator/bindex.html', array('id'=>$id));
 
 //check if data exists in the sub-plugin table, create new data if doesn't exist
+
+//Fill and print the form
+$pagetitle = $book->name . ": " . 
+
+$PAGE->set_title($book->name);
+$PAGE->set_heading($course->fullname);
+
+echo $OUTPUT->header();
+echo $OUTPUT->heading($book->name);
 
 if ( !($DB->record_exists('booktool_validator', array('bookid'=>$book->id))) ) {
     
@@ -61,108 +69,23 @@ if ( !($DB->record_exists('booktool_validator', array('bookid'=>$book->id))) ) {
             $record->faults = cnt_faults($book, $chapter);
             $DB->insert_record('booktool_validator', $record, false);
 
-        } 
+            $link = new action_link();
+            $link->url = new moodle_url('/mod/book/tool/validator/bcindex.php', 
+            array('id' => $book->id, 'chapterid' => $chapter->id));
+            $link->text = get_string('val_chapter', 'booktool_validator'); // Required
+            echo $OUTPUT->link($link);
 
-        $record->validated = 1;
-        $record->faults = 0;
-        $DB->insert_record('booktool_validator', $record, false);        
-    }
+        } else {
 
-} else {
-    $chapterids = $DB->get_records_sql('SELECT id FROM {book_chapters} WHERE bookid = ?', array($book->id));
-
-    foreach ($chapterids as $chapter) {
-
-        $record = new stdClass();
-
-        if ( !($DB->record_exists('booktool_validator', array('bookid'=>$book->id, 'chapterid'=>$chapter->id))) ) {
-
-            $record->bookid = $book->id;
-            $record->chapterid = $chapter;
-
-            if (!chapter_checkvalidation($book, $chapter)) {
-
-            $record->validated = 0;
-            $record->faults = cnt_faults($book->id, $chapter);
+            $record->validated = 1;
+            $record->faults = 0;
             $DB->insert_record('booktool_validator', $record, false);
 
-            }
-
-        $record->validated = 1;
-        $record->faults = 0;
-        $DB->insert_record('booktool_validator', $record, false);
-
         }
-    }
-}
 
-
-$mform = new chapter_validation_form(null, array('book'=>$book, 'chapter'=>$chapter));
-
-//If data submitted, process and store
-
-if ($mform->is_cancelled()) {
-    if (empty($chapter->id)) {
-        redirect("view.php?id=$cm->id");
-    } else {
-        redirect("view.php?id=$cm->id&chapterid=$chapter->id");
+                
     }
 
-} else if ($data = $mform->get_data()) {
-    
-    if ($data->id) {
-        //store the files
-        $data->timemodified = time();
-        $data = file_postupdate_standard_editor($data, 'content', $options, $context, 'mod_book', 'chapter', $data->id);
-        //$DB->update_record
-    }
-}
-
-$bookvalidated = book_checkvalidation($book);
-if ($bookvalidated) {
-    echo(get_string('event_book_validated', 'booktool_validator'));
-} else {
-    echo(get_string('event_book_notvalidated', 'booktool_validator'));
-}
-
-//Fill and print the form
-$pagetitle = $book->name . ": " . 
-
-$PAGE->set_title($book->name);
-$PAGE->set_heading($course->fullname);
-
-echo $OUTPUT->header();
-echo $OUTPUT->heading($book->name);
-
-$PAGE->set_button(get_string('event_book_validated', 'booktool_validator'));
-echo $OUTPUT;
-
-$mform->display();
+} 
 
 echo $OUTPUT->footer();
-
-/*$mform = new book_validation_edit_form(null, array('id'=>$id, 'chapterid'=>$chapterid));
-
-if ($mform->is_cancelled()) {
-    if (empty($chapter->id)) {
-        redirect($CFG->wwwroot.'/mod/book/view.php?id=$cm->id');
-    } else {
-        redirect($CFG->wwwroot."/mod/book/view.php?id=$cm->id&chapterid=$chapter->id");
-    }
-} else if ($data = $mform->get_data()) {
-    echo $OUTPUT->header();
-    echo $OUTPUT->heading($book->name);
-    echo $OUTPUT->heading(get_string('validatebook', 'booktool_validator'), 3);
-
-    $is_validated = book_checkvalidation($book);
-
-    if (!$is_validated) {
-        
-    } else {
-        echo get_string('event_chapter_validated', 'booktool_validator');
-    }
-
-    echo $OUTPUT->continue_button(new moodle_url('/mod/book/view.php', array('id'=>$id)));
-    echo $OUTPUT->footer();
-    die;
-}*/
