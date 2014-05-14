8933a2e951675aa6131da5e01b31ad05745f9af3
Moirae <is.moirae@gmail.com>
2014-05-14 14:15:14 +0200

    Croatian language
    Created Croatian language file

---
+++ tool/validator/lang/hr/booktool_validator.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * validator booktool language strings in Croatian
+ *
+ * @package    booktool_validator
+ * @copyright  2014 Ivana Skelic, Hrvoje Golcic 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$string['event_chapter_validated'] = 'U poglavlju nema validacijskih problema.';
+$string['event_chapter_notvalidated'] = 'Poglavlje nije prošlo validacijsku provjeru.';
+$string['event_book_validated'] = 'U knjizi nema validacijskih problema.';
+$string['event_book_notvalidated'] = 'Knjiga nije prošla validacijsku provjeru.';
+$string['validatechapter'] = 'Provjeri poglavlje';
+$string['validatebook'] = 'Provjeri knjigu';
+$string['pluginname'] = 'Provjera sadržaja knjige';
+$string['validator:validated'] = 'provjeri';
+
+$string['buttonvalidate'] = 'Provjeri';
+$string['chapter'] = 'Poglavlje';
+$string['val_chapter'] = 'Provjera poglavlja';
+$string['table'] ='Nedostaje summary u tablicama:';
+$string['image'] = 'Sljedeće slike nemaju opis:';
+$string['nof'] = 'Broj pogrešaka: ';
+
+$string['validate'] = 'Provjeri';
+$string['click'] = 'Klikni ';
